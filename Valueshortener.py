a1fd8307d264233231f33f10265e591b432f2e52
BREgitLEEhub2222 <brelee1842@outlook.com>
2021-03-15 23:36:33 +0000

    Initial commit

---
+++ Valueshortener.py
@@ -0,0 +1,21 @@
+def encrint(integer):
+  integer_ = hex(int(integer))
+  integer = []
+  for i in range(len(integer_)):
+    if len(integer_) >= 2*(i+1)+1:
+      try:
+        print(integer_[2*(i+1)], integer_[2*(i+1)+1])
+        integer.append('0x' + integer_[2*(i+1)] + integer_[2*(i+1)+1])
+      except: 
+          integer.append('0x' + '0' + integer_[2*(i+1)])
+  integers = []
+  for i in integer:
+    i = int(i, 0)
+    integers.append(chr(i))
+  integer = ''.join([str(t) for t in integers])
+  return integer
+def decrint(string):
+  value = '0x'
+  for i in range(len(integer)):
+      value = value + str(hex(ord(integer[i]))).replace('0x', '')
+  return value
