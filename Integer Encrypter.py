2c425bb3b6534ee8db364b94ff935cb939503453
BREgitLEEhub2222 <brelee1842@outlook.com>
2021-03-16 01:35:51 +0000

    optimized

--- Integer Encrypter.py
+++ Integer Encrypter.py
@@ -1,27 +1,19 @@
 def encrint(integer):
   integer_ = hex(int(integer))
-  print(integer_)
   integer = []
-  print((len(integer_)-2+(2*(len(integer_) % 2))) // 2)
   for i in range((len(integer_)-2+(2*(len(integer_) % 2))) // 2):
       print(i)
       try:
-        
         integer.append(int('0x' + integer_[len(integer_)-(2*(i))-2] + integer_[len(integer_)-(2*(i))-1], 0))
-        print('0x' + integer_[len(integer_)-(2*(i))-2] + integer_[len(integer_)-(2*(i))-1])
       except: 
           integer.append(int('0x' + integer_[2], 0))
           print('0x' + integer_[2])
-  print(integer)
   integers = []
   for i in range(len(integer)):
     integers.append(integer[len(integer)-i-1])
   integer = integers
   integers = []
-  print(integer)
-
   for i in integer:
-    print(i)
     integers.append(chr(i))
   integer = ''.join([str(t) for t in integers])
   return integer
